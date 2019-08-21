05df475a3ef91910e1c84bac1efefd8d0e13583f
APCS2nd <APCS2nd@MHS-LT9-22>
2019-08-21 12:16:59 -0700

    Completed Hello World Project

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,11 @@
+//Leo Yang
+//8 16 2019
+//First class iconic Java for APCS
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
